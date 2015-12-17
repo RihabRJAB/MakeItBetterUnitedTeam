92112594ea95643c90650451051af0333ed51e1b
HNBA <belkhiria.hazem@gmail.com>
2015-12-17 21:54:57 +0100

    first modification

---
+++ sample/src/main/java/com/borax12/materialdaterangepickerexample/SplashActivity.java
@@ -0,0 +1,15 @@
+package com.borax12.materialdaterangepickerexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.wdullaer.materialdaterangepicker.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
